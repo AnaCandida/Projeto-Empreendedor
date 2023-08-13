0d446256f538fc93c8af0a1684963472c207ff75
AnaCandida <acandida.quadros@gmail.com>
2023-08-13 01:30:50 -0300

    feat: configura as rotas da aplicação

--- bora_la/bora_la/urls.py
+++ bora_la/bora_la/urls.py
@@ -19,8 +19,11 @@ from django.urls import path, include
 from core.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("logar_usuario", logar_usuario, name="logar_usuario"),
+    path("deslogar_usuario", deslogar_usuario, name="deslogar_usuario"),
     path("cadastrar_usuario", cadastrar_usuario, name="cadastrar_usuario"),
-    path("index", index, name="index"),
+    path("criar_evento", criar_evento, name="criar_evento"),
+    path("listar_eventos", listar_eventos, name="listar_eventos"),
+    path("", index, name="index"),
 ]
