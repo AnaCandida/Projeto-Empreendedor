c2209305171fa8d495fe4e4b72bbf38ad5ad8c40
AnaCandida <acandida.quadros@gmail.com>
2023-08-13 21:29:59 -0300

    tests: atualiza testes  da funcionalidade de login

--- bora_la/core/tests/test_views.py
+++ bora_la/core/tests/test_views.py
@@ -1,13 +1,16 @@
-from django.test import TestCase
+from bora_la.wsgi import *
 from django.contrib.auth.models import User
+from django.test import TestCase
 from django.urls import reverse
 from core.models import Usuario
 from django.test import RequestFactory
-from .views import cadastrar_usuario, logar_usuario, deslogar_usuario, listar_eventos
+from core.views import cadastrar_usuario, logar_usuario, deslogar_usuario, listar_eventos
+import pytest
 
 class TestUsuario(TestCase):
 
     def setUp(self):
+
         self.factory = RequestFactory()
 
     def test_cadastrar_usuario(self):
@@ -27,9 +30,9 @@ class TestUsuario(TestCase):
 
         response = self.client.post(url, data)
 
-        self.assertEqual(response.status_code, 302)  # Redirect status code
+        self.assertEqual(response.status_code, 302)  
 
-        user = get_user_model().objects.get(username="testuser")
+        user = User.objects.get(username="testuser")
         self.assertIsNotNone(user)
 
         usuario = Usuario.objects.filter(django_user=user).first()
@@ -37,7 +40,7 @@ class TestUsuario(TestCase):
         self.assertEqual(usuario.nome, "Test User")
         self.assertEqual(usuario.email, "test@example.com")
         self.assertEqual(usuario.whats, "1234567890")
-        self.assertEqual(usuario.tipo_usuario, "1")
+        self.assertEqual(usuario.tipo_usuario, 1)
         self.assertEqual(usuario.razao_social, "Test Company")
         self.assertIn("comida", usuario.pref_categorias)
         self.assertIn("ar_livre", usuario.pref_categorias)
@@ -49,29 +52,19 @@ class TestUsuario(TestCase):
 
         response = self.client.post(url, data)
 
-        self.assertEqual(response.status_code, 302)  # Redirect status code
+        self.assertEqual(response.status_code, 302) 
 
     def test_deslogar_usuario(self):
         url = reverse("deslogar_usuario")
 
         response = self.client.get(url)
 
-        self.assertEqual(response.status_code, 302)  # Redirect status code
-
-    def test_listar_eventos_authenticated(self):
-        User.objects.create_user(username="testuser", password="testpass123")
-        url = reverse("listar_eventos")
-        self.client.login(username="testuser", password="testpass123")
-
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, 200)  # OK status code
-        self.assertIn("autenticado", response.content.decode())
+        self.assertEqual(response.status_code, 302)
 
     def test_listar_eventos_unauthenticated(self):
         url = reverse("listar_eventos")
 
         response = self.client.get(url)
 
-        self.assertEqual(response.status_code, 200)  # OK status code
-        self.assertNotIn("autenticado", response.content.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNone(response.context.get("tipo_usuario"))
