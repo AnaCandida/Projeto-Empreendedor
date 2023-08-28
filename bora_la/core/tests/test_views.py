614c0ef96b9e42a0c7d265abb47ec29c6f471de8
Emerson Felippini <felippini2015@outlook.com>
2023-08-28 20:23:20 -0300

    chore: teste unitário cadastro_evento

--- bora_la/core/tests/test_views.py
+++ bora_la/core/tests/test_views.py
@@ -2,9 +2,9 @@ from bora_la.wsgi import *
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
-from core.models import Usuario
+from core.models import Usuario, Evento, Categoria
 from django.test import RequestFactory
-from core.views import cadastrar_usuario, logar_usuario, deslogar_usuario, listar_eventos
+from core.views import cadastrar_usuario, logar_usuario, deslogar_usuario, listar_eventos, cadastrar_evento
 import pytest
 
 class TestUsuario(TestCase):
@@ -68,3 +68,56 @@ class TestUsuario(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertIsNone(response.context.get("tipo_usuario"))
+
+# Teste unitário funcionalidade cadastrar_evento #
+class TestCadastrarEvento(TestCase):
+
+    def rf():
+        return RequestFactory()
+
+    def user():
+        user = Usuario.objects.create_user(
+            username="testuser",
+            password="testpass123",
+        )
+        return user
+    
+    def categorias():
+        return [Categoria.objects.create(nome="Categoria1"), Categoria.objects.create(nome="Categoria2")]
+
+    @pytest.fixture
+    def post_data(categorias):
+        return {
+            "nome_evento": "Meu Evento",
+            "descricao_evento": "Descrição do evento",
+            "preco_evento": "50.00",
+            "data_evento": "2023-09-01",
+            "endereco_evento": "Localização do evento",
+            "pref_categorias[]": [str(cat.id) for cat in categorias],
+        }
+
+    @pytest.mark.django_db
+    def test_cadastrar_evento(rf, user, categorias, post_data):
+        request = rf.post("/cadastrar_evento/", data=post_data)
+        request.user = user
+
+        response = cadastrar_evento(request)
+
+        assert response.status_code == 302  # Verifique se o redirecionamento ocorreu
+        eventos_criados = Evento.objects.filter(nome="Meu Evento")
+        assert eventos_criados.count() == 1
+        evento_criado = eventos_criados.first()
+        assert evento_criado.organizador_id == Usuario  # Substitua pelo campo correto
+        assert list(evento_criado.categorias_id.all()) == categorias
+
+        # Teste para o caso em que a criação do evento falha
+        with pytest.raises(Exception):
+            request_with_error = rf.post("/cadastrar_evento/", data={})
+            request_with_error.user = user
+            cadastrar_evento(request_with_error)
+
+        # Teste para o caso em que o método não é POST
+        request_get = rf.get("/cadastrar_evento/")
+        request_get.user = user
+        response_get = cadastrar_evento(request_get)
+        assert response_get.status_code == 200
