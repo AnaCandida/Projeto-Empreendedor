2dba60311f33fa4765707bb22943414560dab481
AnaCandida <acandida.quadros@gmail.com>
2023-08-27 22:10:39 -0300

    feat: cria script para popular dados de categorias default no banco de dados

---
+++ bora_la/populate_db.py
@@ -0,0 +1,22 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nomedoseuprojeto.settings")
+django.setup()
+
+from core.models import Categoria
+
+
+def populate_categories():
+    categorias = ["Comida", "Ar Livre", "Negócio Local", "Cultura", "Esporte", "Música"]
+
+    for categoria_nome in categorias:
+        categoria, created = Categoria.objects.get_or_create(nome=categoria_nome)
+        if created:
+            print(f"Criada categoria: {categoria.nome}")
+        else:
+            print(f"Categoria já existe: {categoria.nome}")
+
+
+if __name__ == "__main__":
+    populate_categories()
