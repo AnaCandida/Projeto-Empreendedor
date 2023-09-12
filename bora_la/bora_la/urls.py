a899a5dd961d74d0f6e2fa46162ec64aecb10499
AnaCandida <acandida.quadros@gmail.com>
2023-09-11 21:13:20 -0300

    feat: inicia a funcionalidade de edicao de conta

--- bora_la/bora_la/urls.py
+++ bora_la/bora_la/urls.py
@@ -10,13 +10,13 @@ urlpatterns = [
     path("logar_usuario", logar_usuario, name="logar_usuario"),
     path("deslogar_usuario", deslogar_usuario, name="deslogar_usuario"),
     path("cadastrar_usuario", cadastrar_usuario, name="cadastrar_usuario"),
+    path("editar_usuario/<int:id>", editar_usuario, name="editar_usuario"),
     path("cadastrar_evento", cadastrar_evento, name="cadastrar_evento"),
     path("listar_eventos", listar_eventos, name="listar_eventos"),
     path("visualizar_evento/<int:id>", visualizar_evento, name="visualizar_evento"),
     path("editar_evento/<int:id>", editar_evento, name="editar_evento"),
     path("meus_eventos", meus_eventos, name="meus_eventos"),
     path("filtrar_eventos", filtrar_eventos, name="filtrar_eventos"),
-
     path("", index, name="index"),
 ]
 
